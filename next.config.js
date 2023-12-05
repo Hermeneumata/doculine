c23f7fb0b1f8f825ec050cf5618e892cb3363d21
Dominik Stodolny <dominik@hermeneumata.com>
2023-12-05 08:23:25 +0000

    fix module not found

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
-    domains: ['avatars.githubusercontent.com', 'avatar.vercel.sh', 'tailwindui.com', 'images.unsplash.com']
-  }
-}
+    domains: [
+      "avatars.githubusercontent.com",
+      "avatar.vercel.sh",
+      "tailwindui.com",
+      "images.unsplash.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
