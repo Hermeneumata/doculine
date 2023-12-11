730fea5a59132c1e292b2694997ae596506373d9
Dominik Stodolny <dominik@hermeneumata.com>
2023-12-11 11:24:08 +0000

    use file link

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -50,8 +50,7 @@ function TimelineRecord({
       </p>
       <p className="mb-4 text-base font-normal text-gray-500">{description}</p>
       <a
-        href={downloadLink}
-        download
+        href={`file://${downloadLink}`}
         className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700"
       >
         <svg
