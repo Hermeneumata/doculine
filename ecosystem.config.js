a7ce66fac49f6c18ccf8a18cacf2756373d834aa
Dominik Stodolny <dominik@hermeneumata.com>
2024-01-31 21:53:36 +0000

    update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "doculine",
+      name: "doculine-app",
       script: "./node_modules/next/dist/bin/next",
       args: "start -p " + (process.env.PORT || 3000),
       watch: false,
